ff6f8cc50430f6ea8fcb0097a90e77ba07a45a98
XDagain-jpg <fan.xind@northeastern.edu>
2025-01-22 16:32:42 -0500

    Add files via upload

--- java/src/main/java/org/example/InsertionSort.java
+++ java/src/main/java/org/example/InsertionSort.java
@@ -9,8 +9,14 @@ public class InsertionSort {
     }
 
     private void insert(int i, Comparable[] a) {
-        //Inserts the 'Transition element' into its correct position in the sorted portion of the array.
-        //TODO: TO BE IMPLEMENTED
+    
+        for (int j = i-1; j>-1; j--) {
+        	if (a[j].compareTo(a[j+1])>=0) {
+        		swap(a, j, j+1);
+        	}
+        	else break;
+        }
+        
     }
 
     private void swap(Object[] a, int i, int j) {
